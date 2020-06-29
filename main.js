925e99bae8262faa25b23bc35d65fc72a38f7232
PapArturo <centinela90@gmail.com>
2020-06-29 18:59:02 -0500

    Added synth sounds

--- main.js
+++ main.js
@@ -8,37 +8,103 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
 var modeOne = [{
     keyName: 'Q',
-    keyCode: 81,
     id: 'Miau',
-    url: ''
+    url: 'https://raw.githubusercontent.com/arturogascon/drum_machine/master/sounds/Q.mp3'
 }, {
     keyName: 'W',
-    keyCode: 87,
     id: 'Blah!',
-    url: ''
+    url: 'https://raw.githubusercontent.com/arturogascon/drum_machine/master/sounds/W.mp3'
 }, {
-    //Continuar construyendo este objeto!!!
-
+    keyName: 'E',
+    id: 'Buruburubu!',
+    url: 'https://raw.githubusercontent.com/arturogascon/drum_machine/master/sounds/E.mp3'
+}, {
+    keyName: 'A',
+    id: 'Mouth-explosion',
+    url: 'https://raw.githubusercontent.com/arturogascon/drum_machine/master/sounds/A.mp3'
+}, {
+    keyName: 'S',
+    id: 'Ahhhrgh!',
+    url: 'https://raw.githubusercontent.com/arturogascon/drum_machine/master/sounds/S.mp3'
+}, {
+    keyName: 'D',
+    id: 'Jijijijijiji!',
+    url: 'https://raw.githubusercontent.com/arturogascon/drum_machine/master/sounds/D.mp3'
+}, {
+    keyName: 'Z',
+    id: 'Bazinnngah',
+    url: 'https://raw.githubusercontent.com/arturogascon/drum_machine/master/sounds/Z.mp3'
+}, {
+    keyName: 'X',
+    id: 'Spongebob-laugh',
+    url: 'https://raw.githubusercontent.com/arturogascon/drum_machine/master/sounds/X.mp3'
+}, {
+    keyName: 'C',
+    id: 'Mmmmmmmh!',
+    url: 'https://raw.githubusercontent.com/arturogascon/drum_machine/master/sounds/C.mp3'
 }];
+
+//Button active style
+var buttonPressed = {
+    backgroundColor: '#01A7C2'
+};
+var buttonUnpressed = {
+    backgroundColor: 'transparent'
+};
 
 var Drum_Machine = function (_React$Component) {
     _inherits(Drum_Machine, _React$Component);
 
-    function Drum_Machine() {
+    function Drum_Machine(props) {
         _classCallCheck(this, Drum_Machine);
 
-        return _possibleConstructorReturn(this, (Drum_Machine.__proto__ || Object.getPrototypeOf(Drum_Machine)).apply(this, arguments));
+        var _this = _possibleConstructorReturn(this, (Drum_Machine.__proto__ || Object.getPrototypeOf(Drum_Machine)).call(this, props));
+
+        _this.state = {
+            display: 'Press keyboard or clic!',
+            volume: 0.7,
+            displayMode: 'Click me to change mode'
+        };
+        _this.handleUpdateDisplay = _this.handleUpdateDisplay.bind(_this);
+        _this.handleVolumeAdjustment = _this.handleVolumeAdjustment.bind(_this);
+        return _this;
     }
 
     _createClass(Drum_Machine, [{
+        key: 'handleUpdateDisplay',
+        value: function handleUpdateDisplay(name) {
+            this.setState({
+                display: name
+            });
+        }
+    }, {
+        key: 'handleVolumeAdjustment',
+        value: function handleVolumeAdjustment(e) {
+            this.setState({
+                display: 'Volume: ' + e.target.value * 10,
+                volume: e.target.value
+            });
+        }
+    }, {
         key: 'render',
         value: function render() {
             return React.createElement(
                 'div',
                 { id: 'drum-machine' },
-                React.createElement(Display, null),
-                React.createElement(Keys, null),
-                React.createElement('input', { type: 'range' })
+                React.createElement(
+                    'div',
+                    { id: 'display' },
+                    React.createElement(
+                        'p',
+                        null,
+                        this.state.display
+                    )
+                ),
+                React.createElement(Keys, { updateDisplay: this.handleUpdateDisplay, volume: this.state.volume }),
+                React.createElement('input', {
+                    type: 'range', value: this.state.volume, min: '0', max: '1', step: '0.1',
+                    onChange: this.handleVolumeAdjustment }),
+                React.createElement(Worm, { displayMode: this.state.displayMode })
             );
         }
     }]);
@@ -46,78 +112,109 @@ var Drum_Machine = function (_React$Component) {
     return Drum_Machine;
 }(React.Component);
 
-function Display(props) {
-    return React.createElement(
-        'div',
-        { id: 'display' },
-        React.createElement(
-            'p',
-            null,
-            'Soy un display'
-        )
-    );
-}
+var Keys = function (_React$Component2) {
+    _inherits(Keys, _React$Component2);
 
-function Keys(props) {
-    return React.createElement(
-        'div',
-        { id: 'keys-container' },
-        React.createElement(
-            'button',
-            { className: 'drum-pad' },
-            'Q',
-            React.createElement('audio', { id: 'Q', className: 'clip', src: '' })
-        ),
-        React.createElement(
-            'button',
-            { className: 'drum-pad' },
-            'W',
-            React.createElement('audio', { id: 'W', className: 'clip', src: '' })
-        ),
-        React.createElement(
-            'button',
-            { className: 'drum-pad' },
-            'E',
-            React.createElement('audio', { id: 'E', className: 'clip', src: '' })
-        ),
-        React.createElement(
-            'button',
-            { className: 'drum-pad' },
-            'A',
-            React.createElement('audio', { id: 'A', className: 'clip', src: '' })
-        ),
-        React.createElement(
-            'button',
-            { className: 'drum-pad' },
-            'S',
-            React.createElement('audio', { id: 'S', className: 'clip', src: '' })
-        ),
-        React.createElement(
-            'button',
-            { className: 'drum-pad' },
-            'D',
-            React.createElement('audio', { id: 'D', className: 'clip', src: '' })
-        ),
-        React.createElement(
-            'button',
-            { className: 'drum-pad' },
-            'Z',
-            React.createElement('audio', { id: 'Z', className: 'clip', src: '' })
-        ),
-        React.createElement(
-            'button',
-            { className: 'drum-pad' },
-            'X',
-            React.createElement('audio', { id: 'X', className: 'clip', src: '' })
-        ),
-        React.createElement(
-            'button',
-            { className: 'drum-pad' },
-            'C',
-            React.createElement('audio', { id: 'C', className: 'clip', src: '' })
-        )
-    );
-}
+    function Keys(props) {
+        _classCallCheck(this, Keys);
+
+        var _this2 = _possibleConstructorReturn(this, (Keys.__proto__ || Object.getPrototypeOf(Keys)).call(this, props));
+
+        _this2.playSound = _this2.playSound.bind(_this2);
+        _this2.handleKeypress = _this2.handleKeypress.bind(_this2);
+        _this2.handleClick = _this2.handleClick.bind(_this2);
+        return _this2;
+    }
+
+    _createClass(Keys, [{
+        key: 'componentDidMount',
+        value: function componentDidMount() {
+            document.addEventListener('keydown', this.handleKeypress);
+        }
+    }, {
+        key: 'handleKeypress',
+        value: function handleKeypress(e) {
+            var keypressID = e.key.toUpperCase(); //id of the <audio> DOM element
+            var keypress = document.getElementById(keypressID);
+            this.playSound(keypress);
+        }
+    }, {
+        key: 'handleClick',
+        value: function handleClick(e) {
+            var sound = e.currentTarget.children[0];
+            this.playSound(sound);
+        }
+    }, {
+        key: 'playSound',
+        value: function playSound(sound) {
+            Object.assign(sound.parentElement.style, buttonPressed);
+            sound.currentTime = 0;
+            sound.volume = this.props.volume;
+            sound.play();
+            setTimeout(function () {
+                return Object.assign(sound.parentElement.style, buttonUnpressed);
+            }, 500);
+            this.props.updateDisplay(sound.parentElement.id.replace(/-/g, ' '));
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            var _this3 = this;
+
+            var keyBoard = modeOne.map(function (element, i, modeArray) {
+                return React.createElement(
+                    'button',
+                    { className: 'drum-pad', onClick: _this3.handleClick, id: modeArray[i].id },
+                    modeArray[i].keyName,
+                    React.createElement('audio', { id: modeArray[i].keyName, className: 'clip', src: modeArray[i].url })
+                );
+            });
+
+            return React.createElement(
+                'div',
+                { id: 'keys-container' },
+                keyBoard
+            );
+        }
+    }]);
+
+    return Keys;
+}(React.Component);
+
+var Worm = function (_React$Component3) {
+    _inherits(Worm, _React$Component3);
+
+    function Worm(props) {
+        _classCallCheck(this, Worm);
+
+        return _possibleConstructorReturn(this, (Worm.__proto__ || Object.getPrototypeOf(Worm)).call(this, props));
+    }
+
+    _createClass(Worm, [{
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                { id: 'worm' },
+                React.createElement(
+                    'div',
+                    { id: 'message' },
+                    React.createElement(
+                        'p',
+                        null,
+                        this.props.displayMode
+                    ),
+                    React.createElement('div', null)
+                ),
+                React.createElement('img', { src: './styles/img/worm.png', alt: 'Friendly Worm' })
+            );
+        }
+    }]);
+
+    return Worm;
+}(React.Component);
 
 //Renderiza React en el DOM:
+
+
 ReactDOM.render(React.createElement(Drum_Machine, null), document.getElementById('app'));
--- src/main.js
+++ src/main.js
@@ -1,103 +1,159 @@
 const modeOne = [{
-        keyName: 'Q',
-        keyCode: 81,
-        id: 'Miau',
-        url: ''
-    },{
-        keyName: 'W',
-        keyCode: 87,
-        id: 'Blah!',
-        url: ''
-    },{
-        //Continuar construyendo este objeto!!!
+    keyName: 'Q',
+    id: 'Miau',
+    url: 'https://raw.githubusercontent.com/arturogascon/drum_machine/master/sounds/Q.mp3'
+}, {
+    keyName: 'W',
+    id: 'Blah!',
+    url: 'https://raw.githubusercontent.com/arturogascon/drum_machine/master/sounds/W.mp3'
+}, {
+    keyName: 'E',
+    id: 'Buruburubu!',
+    url: 'https://raw.githubusercontent.com/arturogascon/drum_machine/master/sounds/E.mp3'
+}, {
+    keyName: 'A',
+    id: 'Mouth-explosion',
+    url: 'https://raw.githubusercontent.com/arturogascon/drum_machine/master/sounds/A.mp3'
+}, {
+    keyName: 'S',
+    id: 'Ahhhrgh!',
+    url: 'https://raw.githubusercontent.com/arturogascon/drum_machine/master/sounds/S.mp3'
+}, {
+    keyName: 'D',
+    id: 'Jijijijijiji!',
+    url: 'https://raw.githubusercontent.com/arturogascon/drum_machine/master/sounds/D.mp3'
+}, {
+    keyName: 'Z',
+    id: 'Bazinnngah',
+    url: 'https://raw.githubusercontent.com/arturogascon/drum_machine/master/sounds/Z.mp3'
+}, {
+    keyName: 'X',
+    id: 'Spongebob-laugh',
+    url: 'https://raw.githubusercontent.com/arturogascon/drum_machine/master/sounds/X.mp3'
+}, {
+    keyName: 'C',
+    id: 'Mmmmmmmh!',
+    url: 'https://raw.githubusercontent.com/arturogascon/drum_machine/master/sounds/C.mp3'
+}
+];
 
+//Button active style
+const buttonPressed = {
+    backgroundColor: '#01A7C2'
+}
+const buttonUnpressed = {
+    backgroundColor: 'transparent'
+}
+
+class Drum_Machine extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            display: 'Press keyboard or clic!',
+            volume: 0.7,
+            displayMode: 'Click me to change mode'
+        }
+        this.handleUpdateDisplay = this.handleUpdateDisplay.bind(this);
+        this.handleVolumeAdjustment = this.handleVolumeAdjustment.bind(this);
     }
 
-]
+    handleUpdateDisplay(name) {
+        this.setState({
+            display: name
+        });
+    }
 
-class Drum_Machine extends React.Component{
-    render(){
+    handleVolumeAdjustment(e) {
+        this.setState({
+            display: `Volume: ${e.target.value * 10}`,
+            volume: e.target.value
+        })
+    }
+
+    render() {
         return (
             <div id="drum-machine">
                 {/* <h1>Mensaje del gusanito</h1> */}
-                <Display/>
-                <Keys/>
-                <input type="range"/>          
+                <div id="display">
+                    <p>{this.state.display}</p>
+                </div>
+                <Keys updateDisplay={this.handleUpdateDisplay} volume={this.state.volume} />
+                <input
+                    type="range" value={this.state.volume} min="0" max="1" step="0.1"
+                    onChange={this.handleVolumeAdjustment} />
+                <Worm displayMode={this.state.displayMode}/>                
             </div>
         );
     }
 }
 
-function Display(props){
-    return (
-        <div id="display">
-            <p>Soy un display</p>
-        </div>
-    );
+class Keys extends React.Component {
+    constructor(props) {
+        super(props);
+        this.playSound = this.playSound.bind(this);
+        this.handleKeypress = this.handleKeypress.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    componentDidMount() {
+        document.addEventListener('keydown', this.handleKeypress)
+    }
+
+    handleKeypress(e) {
+        const keypressID = e.key.toUpperCase(); //id of the <audio> DOM element
+        const keypress = document.getElementById(keypressID);
+        this.playSound(keypress);
+    }
+
+    handleClick(e) {
+        const sound = e.currentTarget.children[0];
+        this.playSound(sound);
+    }
+
+    playSound(sound) {
+        Object.assign(sound.parentElement.style, buttonPressed);
+        sound.currentTime = 0;
+        sound.volume = this.props.volume;
+        sound.play();
+        setTimeout(() => Object.assign(sound.parentElement.style, buttonUnpressed), 500);
+        this.props.updateDisplay(sound.parentElement.id.replace(/-/g, ' '));
+    }
+
+    render() {
+        let keyBoard = modeOne.map((element, i, modeArray) => {
+            return (
+                <button className="drum-pad" onClick={this.handleClick} id={modeArray[i].id}>
+                    {modeArray[i].keyName}
+                    <audio id={modeArray[i].keyName} className="clip" src={modeArray[i].url} />
+                </button>
+            );
+        });
+
+        return (
+            <div id="keys-container">
+                {keyBoard}
+            </div>
+        );
+    }
 }
 
-function Keys(props){
-    return(
-        <div id="keys-container">
-            <button className="drum-pad">
-                Q
-                <audio id="Q" className="clip" src="">
-
-                </audio>
-            </button>
-            <button className="drum-pad">
-                W
-                <audio id="W" className="clip" src="">
-
-                </audio>
-            </button>
-            <button className="drum-pad">
-                E
-                <audio id="E" className="clip" src="">
-
-                </audio>
-            </button>
-            <button className="drum-pad">
-                A
-                <audio id="A" className="clip" src="">
-
-                </audio>
-            </button>
-            <button className="drum-pad">
-                S
-                <audio id="S" className="clip" src="">
-
-                </audio>
-            </button>
-            <button className="drum-pad">
-                D
-                <audio id="D" className="clip" src="">
-
-                </audio>
-            </button>
-            <button className="drum-pad">
-                Z
-                <audio id="Z" className="clip" src="">
-
-                </audio>
-            </button>
-            <button className="drum-pad">
-                X
-                <audio id="X" className="clip" src="">
-
-                </audio>
-            </button>
-            <button className="drum-pad">
-                C
-                <audio id="C" className="clip" src="">
-
-                </audio>
-            </button>
-        </div>
-    );
+    
+class Worm extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div id="worm">
+                <div id="message">
+                    <p>{this.props.displayMode}</p>
+                    <div/>
+                </div>
+                <img src="./styles/img/worm.png" alt="Friendly Worm"/>
+            </div>
+        );
+    }
 }
-
-
 
 //Renderiza React en el DOM:
-ReactDOM.render(<Drum_Machine/>, document.getElementById('app'));
+ReactDOM.render(<Drum_Machine />, document.getElementById('app'));
